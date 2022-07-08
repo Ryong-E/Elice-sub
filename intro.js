38987fd1125d672d35bebf5ae9506a0b8c49e540
handongryong <think8867@gmail.com>
2022-07-08 13:26:57 +0900

    done

---
+++ intro.js
@@ -0,0 +1,21 @@
+function js() {
+  document.querySelector("#myinfor").innerHTML =
+    "React.js,Node.js,Nest.js,TypeScript,emotion,SCSS,Jest,wabpack에 관심이있습니다";
+}
+
+function python() {
+  document.querySelector("#myinfor").innerHTML =
+    "인공지능을 적용하여서 새로운 것을 만들어 볼 수 있는 프로젝트가 기대가 됩니다";
+}
+
+function trackTalk() {
+  document.querySelector("#myinfor").innerHTML =
+    "비대면이지만 친하게 지낼 수 있으면 좋겠고 많은 도움 주셨으면 감사하겠습니다 저도 도움을 줄 수 있다면 많이 도와드리겠습니다!";
+}
+
+function hobby() {
+  document.querySelector("#myinfor").innerHTML =
+    "Github : https://github.com/Ryong-E<br>\
+       #운동#탑건#노래\
+      ";
+}
